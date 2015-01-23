2d413a51aadae22ab686a10d1e4c18910be81240
Shay Deacy <shaydeacy@gmail.com>
2015-01-23 17:33:12 +0000

    initial commit of my code

--- js/script.js
+++ js/script.js
@@ -11,64 +11,60 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
-
-    $greeting.text('So, you want to live at ' + address + '?');
-
     // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    // YOUR CODE GOES HERE!
+    var street = $('#street').val();
+    var city   = $('#city').val();
+    var src = 'https://maps.googleapis.com/maps/api/streetview?size=800x400&location="' + street + ',' + city;
+    var nytimesURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+city+"&api-key=07aaafa9eae96a0f7f5830c10978c112:12:70995210"
+
+    $body.append('<img class="bgimg" src='+src+'>');
+    $greeting.text(city+'?, really???');
+    $.getJSON(nytimesURL, function( data ) {
+
+        var articles = [];
+
+        for(var i = 0; i< (data.response.docs).length;i++) {
+            var headline = data.response.docs[i].headline.main;
+            var leadParagraph = data.response.docs[i].lead_paragraph;
+            if(headline && leadParagraph){
+                articles.push( "<li class='article'>" + "<h3>" + headline + "</h3>" + "<p>"+leadParagraph +"</p>"+ "</li>" );
+            }
+        }
 
 
-    // load nytimes
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633'
-    $.getJSON(nytimesUrl, function(data){
-
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
-
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
-
-    }).error(function(e){
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+        $( "<ul/>", {
+            "class": "article",
+            html: articles.join( "" )
+        }).appendTo( '#nytimes-articles' );
+    }).error(function() {
+        $nytElem.text("Can't get articles right now...Sorry")
     });
 
-
-
-    // load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
     var wikiRequestTimeout = setTimeout(function(){
-        $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
+        $wikiElem.text("Can't get to wiki right now...Sorry")
 
+    },8000);
+
+    var wikiURL = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+city+"&format=json&callback=?";
     $.ajax({
-        url: wikiUrl,
+        url: wikiURL,
         dataType: "jsonp",
-        jsonp: "callback",
-        success: function( response ) {
-            var articleList = response[1];
-
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            };
-
+        success: function (data) {
+            console.log(data[1] );
+            var articleList = data[1];
+            for(var i=0;i<articleList.length;i++) {
+                articleString = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleString;
+                $wikiElem.append('<li><a href="'+url+'">'+articleString+'</a></li>');
+            }
             clearTimeout(wikiRequestTimeout);
         }
     });
-
-    return false;
-};
+        return false;
+}
 
 $('#form-container').submit(loadData);
 
-// loadData();
+ //loadData();
